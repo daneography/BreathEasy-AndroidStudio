a1437564bea8250cedabdbe225ddcec7015f9aa1
daneography <acena.dane@gmail.com>
2017-06-06 14:06:03 -0400

    version 0.1

--- app/src/main/java/uofsc/com/breatheasy/welcomeScreen.java
+++ app/src/main/java/uofsc/com/breatheasy/welcomeScreen.java
@@ -1,7 +1,12 @@
 package uofsc.com.breatheasy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class welcomeScreen extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class welcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 }
