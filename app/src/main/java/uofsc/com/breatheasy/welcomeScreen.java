c1c6abc24dc47fb33fa3f79ea0e378a32bf2d522
daneography <acena.dane@gmail.com>
2017-06-06 13:21:12 -0400

    Initial Commit

---
+++ app/src/main/java/uofsc/com/breatheasy/welcomeScreen.java
@@ -0,0 +1,13 @@
+package uofsc.com.breatheasy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
